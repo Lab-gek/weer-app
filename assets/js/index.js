5799dd3bbb586f55164ba058eeaf172dccc07d8d
lab-gek <131662563+Lab-gek@users.noreply.github.com>
2024-05-22 23:13:57 +0200

    Ik haat dit nu

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,20 +2,22 @@ Regengrafiek = document.getElementById('Regengrafiek');
 TitleVoorspel = document.getElementById('TitleVoorspel');
 Voorspeltxt = document.getElementById('Voorspeltxt');
 Neerslagimg = document.getElementById('Neerslagimg');
+
 buienradar.load(function () {
     buienradar.showRainChart('Regengrafiek');
     buienradar.forecast.report.title('TitleVoorspel')
     buienradar.forecast.report.text('Voorspeltxt')
     buienradar.map('Neerslagimg')
     //dag info in variablen
+    const parent = document.getElementById()
     for (let i = 0; i < 5; i++) {
-         buienradar.forecast.fiveDays.temperature.min(`tempmd${i}` , i)
-         buienradar.forecast.fiveDays.temperature.max(`tempmx${i}` , i)
-         buienradar.forecast.fiveDays.rain.chance(`rainch${i}` , i)
-         buienradar.forecast.fivedays.rain.mmMin(`rainmmMin${i}` , i)
-         buienradar.forecast.fivedays.rain.mmMax(`rainmmMax${i}` , i)
-         buienradar.forecast.fiveDays.sunChance(`sunch${i}` , i)    
-         buienradar.forecast.fiveDays.wind.direction(`winddir${i}` , i)
-         buienradar.forecast.fiveDays.wind.speed(`windspeed${i}` , i)
-         buienradar.forecast.fiveDays.icon(`icon${i}` , i)
-});
+        let tempmd${i} = document.getElementById(`tempmd${i}`)
+        buienradar.forecast.fiveDays.temperature.min(`tempmd${i}` , i)
+        buienradar.forecast.fiveDays.temperature.max(`tempmx${i}` , i)
+        buienradar.forecast.fiveDays.rain.chance(`rainch${i}` , i)
+        buienradar.forecast.fivedays.rain.mmMin(`rainmmMin${i}` , i)
+        buienradar.forecast.fivedays.rain.mmMax(`rainmmMax${i}` , i)
+        buienradar.forecast.fiveDays.sunChance(`sunch${i}` , i)    
+        buienradar.forecast.fiveDays.wind.direction(`winddir${i}` , i)
+        buienradar.forecast.fiveDays.wind.speed(`windspeed${i}` , i)
+        buienradar.forecast.fiveDays.icon(`icon${i}` , i)}})
