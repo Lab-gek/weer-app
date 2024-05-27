2521c2e22716a56c7bb53dbfb3ac8658095fd1b2
lab-gek <131662563+Lab-gek@users.noreply.github.com>
2024-05-27 14:20:42 +0200

    big one
    Co-authored-by: Tijtteker2019 <Tijtteker2019@users.noreply.github.com>
    Co-authored-by: KillerKast7 <KillerKast7@users.noreply.github.com>

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,34 +2,46 @@ Regengrafiek = document.getElementById('Regengrafiek');
 TitleVoorspel = document.getElementById('TitleVoorspel');
 Voorspeltxt = document.getElementById('Voorspeltxt');
 Neerslagimg = document.getElementById('Neerslagimg');
+Weericonnav = document.getElementById('Weericonnav');
+legal= document.getElementById('legal');
+zonop = document.getElementById('zonop');
 
 buienradar.load(function () {
     buienradar.showRainChart('Regengrafiek');
     buienradar.forecast.report.title('TitleVoorspel')
     buienradar.forecast.report.text('Voorspeltxt')
     buienradar.map('Neerslagimg')
+    buienradar.currentLocation.icon('Weericonnav')
+    buienradar.buienradar.copyright('legal')
+    buienradar.sunrise('zonop')
     //dag info in variablen
-    for (let i = 0; i < 5; i++) {
-        // maak de variablen aan
-        tempmd[i] = document.getElementById('tempmd'+i);
-        //tempmd[i] = document.getElementById(`tempmx${i}`);
-        //rainch[i] = document.getElementById(`rainch${i}`);
-        //rainmmMin[i] = document.getElementById(`rainmmMin${i}`);
-        //rainmmMax[i] = document.getElementById(`rainmmMax${i}`);
-        //sunch[i] = document.getElementById(`sunch${i}`);
-        //winddir[i] = document.getElementById(`winddir${i}`);
-        //windspeed[i] = document.getElementById(`windspeed${i}`);
-        //icon[i] = document.getElementById(`icon${i}`);
-        //vul in de variablen
-        buienradar.forecast.fiveDays.temperature.min(`tempmd${i}` , i)
-        buienradar.forecast.fiveDays.temperature.max(`tempmx${i}` , i)
-        buienradar.forecast.fiveDays.rain.chance(`rainch${i}` , i)
-        buienradar.forecast.fivedays.rain.mmMin(`rainmmMin${i}` , i)
-        buienradar.forecast.fivedays.rain.mmMax(`rainmmMax${i}` , i)
-        buienradar.forecast.fiveDays.sunChance(`sunch${i}` , i)    
-        buienradar.forecast.fiveDays.wind.direction(`winddir${i}` , i)
-        buienradar.forecast.fiveDays.wind.speed(`windspeed${i}` , i)
-        buienradar.forecast.fiveDays.icon(`icon${i}` , i)
+    let tempmd = {};
+    let rainmmMax = {};
+    let sunch = {};
+    let winddir = {};
+    let windspeed = {};
+    let icon = {};
+    
+    for(let i = 0; i < 5; i++) {
+        tempmd[`tempmd`+i] = buienradar.forecast.fiveDays.temperature.min(i);
+        document.getElementById(`tempmd`+i).innerText = tempmd[`tempmd`+i];
+
+        console.log(tempmd[`tempmd`+i]);
+        rainmmMax[`rainmmMax`+i] = buienradar.forecast.fiveDays.rain.mmMax(i);
+        document.getElementById(`rainmmMax`+i).innerText = rainmmMax[`rainmmMax`+i];
+    
+        sunch[`sunch`+i] = buienradar.forecast.fiveDays.sunchance(i);
+        document.getElementById(`sunch`+i).innerText = sunch[`sunch`+i];
+    
+        winddir[`winddir`+i] = buienradar.forecast.fiveDays.wind.direction(i);
+        document.getElementById(`winddir`+i).innerText = winddir[`winddir`+i];
+    
+        windspeed[`windspeed`+i] = buienradar.forecast.fiveDays.wind.speed(i);
+        document.getElementById(`windspeed`+i).innerText = windspeed[`windspeed`+i];
+    
+        icon[`icon`+i] = buienradar.forecast.fiveDays.icon(i);
+        document.getElementById(`icon`+i).src = icon[`icon`+i];
     }
-}
+    }
+,
 )
---
+++ assets/js/settings.js
@@ -0,0 +1,4 @@
+function DarkModeToggle() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
